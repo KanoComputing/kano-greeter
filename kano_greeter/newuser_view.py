f383ded0aade4ca4b5601d4043bfc33cc5fbd2ac
Albert Casals <skarbat@gmail.com>
2015-04-27 14:09:32 +0100

    fixed module name typo

--- kano_greeter/newuser_view.py
+++ kano_greeter/newuser_view.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# password_view.py
+# newuser_view.py
 #
 # Copyright (C) 2015 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
