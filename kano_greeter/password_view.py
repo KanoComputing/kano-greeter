9b59f433bdaf093c00a8e7698cd87fc66fa3b886
Radu Jipa <radu.t.jipa@gmail.com>
2018-03-28 19:31:44 +0100

    Reverted to pam==0.1.4 converted with py2deb

--- kano_greeter/password_view.py
+++ kano_greeter/password_view.py
@@ -145,12 +145,7 @@ class PasswordView(Gtk.Grid):
         self.password.grab_focus()
 
     def delete_user(self, *args):
-        # When this module was transitioned from pip (0.1.4) to deb (0.4.2-13.1),
-        # the version in Raspbian contained an all caps name for the module.
-        try:
-            import PAM as pam
-        except ImportError:
-            import pam
+        import pam
 
         password_input = Gtk.Entry()
         password_input.set_visibility(False)
